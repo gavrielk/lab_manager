c36fc6adb8572d29b67096da96cb913c70815aa5
gkarasin@gmail.com <gkarasin@gmail.com>
2018-11-06 20:36:58 +0200

    Add first template

--- mysite/lab_manager/settings.py
+++ mysite/lab_manager/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'lab_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['lab_manager/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/lab_manager/views.py
+++ mysite/lab_manager/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the lab_manager index.")
+    template = loader.get_template('lab_manager/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
