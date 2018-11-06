71d66304e60b68ce92993ba2a46214bbf981490b
yuvalva <yuvalv@ceragon.com>
2018-11-06 17:19:22 +0200

    update models.py

--- mysite/lab_manager/models.py
+++ mysite/lab_manager/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Device(models.Model):
+    owner = models.CharField(max_length=20)
+    date_taken = models.DateTimeField('date taken')
+    ip_address= models.GenericIPAddressField(default="192.198.1.1")
