bc435caf2524062590d3583f03eced957ac53e5f
yuvalva <yuvalv@ceragon.com>
2018-11-07 15:28:25 +0200

    fix create_new_device

--- mysite/lab_manager/views.py
+++ mysite/lab_manager/views.py
@@ -124,23 +124,29 @@ def create_new_device(request):
     #  #   request.session[SESSION_KEYS.LOGIN_MESSAGE] = MESSAGES.SESSION_EXPIRED
     #   #  return render(request, login_page, {})
     #
-    # new_device_type = request.POST.get('type' , '')  # new device name
-    # new_device_owner = request.session[SESSION_KEYS.USER_FULL_NAME]
+    new_device_type = request.POST.get('type', '')  # new device name
+    new_device_owner = request.session[SESSION_KEYS.USER_FULL_NAME]
     #
     # new_user = request.session["user_full_name"]  # new device owner is the user
     # new_ip_address = request.POST.get('ip_address', '')  # new ip address
-    # new_location = request.POST.get('location', '')  # new case description
+    #   # new case description
     # new_found_in_release = request.POST.get('found_in_release', '')  # new case found in release
     # new_pattern = request.POST.get('pattern', '')  # new case pattern
-    new_user = "yuval"
+    #new_user = "yuval"
     new_ip_address = "192.198.1.1"
     new_location = "room242"
     new_device_type = "IP20EX"
     new_team = "middleware"
     # create the new device
+    new_user = request.session[SESSION_KEYS.USER_FULL_NAME]
+    new_ip_address = request.POST.get('device_ip', '')
+    new_location = request.POST.get('location', '')
+    new_device_type = request.POST.get('type', '')
+    new_device_team = request.POST.get('team', '')
+    d = Device(owner=new_user, date_taken=timezone.now(), ip_address=new_ip_address, device_location=new_location, type=new_device_type, team=new_device_team)
 
-    d = Device(owner=new_user, date_taken=timezone.now(), ip_address=new_ip_address, device_location=new_location,
-               type=new_device_type, team=new_team)
+    #d = Device(owner=new_user, date_taken=timezone.now(), ip_address=new_ip_address, device_location=new_location,
+    #           type=new_device_type, team=new_team)
     d.save()  # save the new case
     template = loader.get_template('lab_manager/index.html')
     context = {}
