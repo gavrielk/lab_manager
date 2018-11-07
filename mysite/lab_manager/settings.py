15cfc9ac8c3bd46d65b91a311db435dd4bcb1abc
yuvalva <yuvalv@ceragon.com>
2018-11-07 10:05:04 +0200

    settings.py fix installed apps

--- mysite/lab_manager/settings.py
+++ mysite/lab_manager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lab_manager.apps.LabManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,12 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-INSTALLED_APPS = [
-    'lab_manager.apps.LabManagerConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
